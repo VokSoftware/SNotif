03d21143f1d45ef8512df214b7694f792146f638
VokSoftware <31902074+VokSoftware@users.noreply.github.com>
2019-05-23 14:40:10 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace SNotif
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mainToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.remoteSupportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblComputer = new System.Windows.Forms.Label();
+            this.lblIp = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "192.168.1.125";
+            this.notifyIcon1.BalloonTipTitle = "IP Address";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "n/a";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick_1);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mainToolStripMenuItem,
+            this.remoteSupportToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(161, 70);
+            // 
+            // mainToolStripMenuItem
+            // 
+            this.mainToolStripMenuItem.Name = "mainToolStripMenuItem";
+            this.mainToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.mainToolStripMenuItem.Text = "&Main";
+            this.mainToolStripMenuItem.Click += new System.EventHandler(this.mainToolStripMenuItem_Click);
+            // 
+            // remoteSupportToolStripMenuItem
+            // 
+            this.remoteSupportToolStripMenuItem.Name = "remoteSupportToolStripMenuItem";
+            this.remoteSupportToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.remoteSupportToolStripMenuItem.Text = "&Remote Support";
+            this.remoteSupportToolStripMenuItem.Click += new System.EventHandler(this.remoteSupportToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.quitToolStripMenuItem.Text = "&Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Computer Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "IP Address:";
+            // 
+            // lblComputer
+            // 
+            this.lblComputer.AutoSize = true;
+            this.lblComputer.Location = new System.Drawing.Point(104, 20);
+            this.lblComputer.Name = "lblComputer";
+            this.lblComputer.Size = new System.Drawing.Size(24, 13);
+            this.lblComputer.TabIndex = 2;
+            this.lblComputer.Text = "n/a";
+            // 
+            // lblIp
+            // 
+            this.lblIp.AutoSize = true;
+            this.lblIp.Location = new System.Drawing.Point(104, 43);
+            this.lblIp.Name = "lblIp";
+            this.lblIp.Size = new System.Drawing.Size(24, 13);
+            this.lblIp.TabIndex = 3;
+            this.lblIp.Text = "n/a";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 60000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(12, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 25);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "&Close";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(301, 115);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblIp);
+            this.Controls.Add(this.lblComputer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Silver;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Support notifier";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblComputer;
+        private System.Windows.Forms.Label lblIp;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem remoteSupportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mainToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Configuration;
+
+namespace SNotif
+{
+    public partial class Form1 : Form
+    {
+        IntPtr hIcon;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                fUpdateInfo();
+                Hide();
+                this.WindowState = FormWindowState.Minimized;
+                notifyIcon1.Visible = true;
+
+                //Zakazani povoleni Quit
+                if (ConfigurationManager.AppSettings["AllowQuit"]=="0")
+                {
+                    quitToolStripMenuItem.Visible = false;
+                }
+
+                notifyIcon1.ShowBalloonTip(1000);
+                timer1.Enabled = true;
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void fUpdateInfo()
+        {
+            try
+            {
+                String hostName = string.Empty;
+                string sDum = "";
+                hostName = Dns.GetHostName();
+                lblComputer.Text = hostName.ToString();
+
+                IPHostEntry myIP = Dns.GetHostEntry(hostName);
+                IPAddress[] address = myIP.AddressList;
+
+                for (int i = 0; i < address.Length; i++)
+                {
+                    sDum = address[i].ToString();
+                    if (sDum.Contains(ConfigurationManager.AppSettings["IPMask"]) == true)
+                    {
+                        lblIp.Text = sDum;
+                    }
+                }
+                notifyIcon1.Text = "Computer Name: " + lblComputer.Text + Environment.NewLine + "Ip Address: " + lblIp.Text;
+                notifyIcon1.BalloonTipText = "Computer Name: " + lblComputer.Text + Environment.NewLine + "Ip Address: " + lblIp.Text;
+                sDum = lblIp.Text;
+                sDum = sDum.Substring(sDum.Length - 3);
+                sDum.Replace(".", "");
+                CreateTextIcon(sDum);
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        public void CreateTextIcon(string str)
+        {
+            try
+            {
+                Font fontToUse = new Font(ConfigurationManager.AppSettings["IconFont"], Convert.ToInt16(ConfigurationManager.AppSettings["IconFontSize"]), FontStyle.Regular, GraphicsUnit.Pixel);
+                Brush brushToUse = new SolidBrush(Color.FromName(ConfigurationManager.AppSettings["IconColor"].ToString()));
+                Bitmap bitmapText = new Bitmap(16, 16);
+                Graphics g = System.Drawing.Graphics.FromImage(bitmapText);
+                IntPtr hIcon;
+
+                g.Clear(Color.Transparent);
+                g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixelGridFit;
+                g.DrawString(str, fontToUse, brushToUse, -4, -2);
+                hIcon = (bitmapText.GetHicon());
+                notifyIcon1.Icon = System.Drawing.Icon.FromHandle(hIcon);
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.WindowState == FormWindowState.Minimized)
+                {
+                    Hide();
+                    notifyIcon1.Visible = true;
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            try
+            { 
+            }
+            catch(Exception ex)
+            {
+            }
+        }
+
+        private void notifyIcon1_MouseClick_1(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                notifyIcon1.ShowBalloonTip(1000);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                fUpdateInfo();
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void mainToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Show();
+                this.WindowState = FormWindowState.Normal;
+                notifyIcon1.Visible = false;
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Hide();
+                this.WindowState = FormWindowState.Minimized;
+                notifyIcon1.Visible = true;
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void remoteSupportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Spusteni aplikace
+                System.Diagnostics.Process.Start(ConfigurationManager.AppSettings["RSRun"]);
+            }
+            catch(Exception)
+            {
+
+            }
+        }
+    }
+}
